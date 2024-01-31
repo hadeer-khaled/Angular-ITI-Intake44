508a0af20616bd45c4b7cad1b93c978c90927eef
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-01-31 22:44:20 +0200

    Sign Up Gorm

--- E-Commerce/src/app/signup-page/signup-page.component.ts
+++ E-Commerce/src/app/signup-page/signup-page.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './signup-page.component.html',
-  styleUrl: './signup-page.component.css'
+  styleUrl: './signup-page.component.css',
 })
 export class SignupPageComponent {
+  SignUpForm: FormGroup;
+  constructor() {
+    this.SignUpForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      name: new FormControl('', Validators.required),
+      userName: new FormControl('', [
+        Validators.required,
+        this.noSpacesValidator,
+      ]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    });
+  }
+  noSpacesValidator(control: AbstractControl): ValidationErrors | null {
+    if ((control.value as string).indexOf(' ') >= 0) {
+      return { cannotContainSpace: true };
+    }
 
+    return null;
+  }
+
+  handleFormSubmit() {
+    console.log(this.SignUpForm);
+    console.log(this.SignUpForm.value);
+  }
 }
