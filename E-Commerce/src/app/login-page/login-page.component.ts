75382d4d752e4a93cafff46248e82f45e66385fc
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-01-31 20:09:42 +0200

    log in form

--- E-Commerce/src/app/login-page/login-page.component.ts
+++ E-Commerce/src/app/login-page/login-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
-
+  handleFormSubmit(form: any) {
+    console.log(form);
+    console.log(form.value);
+  }
 }
